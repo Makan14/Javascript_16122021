d146d6437ddb2b0ff8b6f3ee18f8a986b33e850b
Makan_14 <m.macalou522@gmail.com>
2021-12-23 16:58:42 +0100

    Base JS 7

--- defi_3/main.js
+++ defi_3/main.js
@@ -1,20 +1,52 @@
 "use strict";
 
-let compteur = setInterval(3000);
-let cpt = document.querySelector("#title");
-console.log(cpt);
-function setInterval(changeTitle, 3000) {
-    
-}
-switch (compteur) {
-  case 1:
-    title.textContent = "salut !";
-    console.log(compteur);
-    break;
-  case 2:
-    title.textContent = "je suis web designer";
-    break;
+let n = 5; //nombre final du compteur
+let cpt = 1; //debut du compteur
+// let duree = 3; //durée en seconde
+// let delta = Math.ceil((duree * 1000) / n); //calcul intervalle de temps entre chaque rafraichissement
+const title = document.getElementById("title");
 
-  default:
-    break;
+function changeTitle() {
+  cpt = ++cpt;
+
+  if (cpt <= n) {
+    console.log(cpt);
+    // if count > 5 count = 0
+    // for (count; count < 5; count++) {
+    // 	console.log(count);
+    // }
+    switch (cpt) {
+      // salut
+      case 1:
+        title.innerHTML = "Salut!";
+        break;
+      // je suis designer web
+      case 2:
+        title.innerHTML = "je suis designer web";
+        break;
+      // je sais concevoir
+      case 3:
+        title.innerHTML = "je sais concevoir";
+        break;
+      // et développer
+      case 4:
+        title.innerHTML = "et développer";
+        break;
+      // des sites web !
+      case 5:
+        title.innerHTML = "des sites web !";
+        cpt = 0;
+        break;
+    }
+  }
+  // setInterval(changeTitle, delta);
 }
+
+setInterval(changeTitle, 3000);
+// ************************************************************************
+
+// keyframes dans le css qui gèrent la fluidité de l'animation.
+// Il suffit de faire une série de condition pour chaque cas et de remplacer le texte dans le HTML,
+// on peut le faire avec switch mais aussi avec une variable
+// compteur qu'on incrémente et des if/else if/else. Plutôt que innerHTML,
+// vous pouvez utiliser innerText pour modifier le texte.
